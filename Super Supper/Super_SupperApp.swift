449ca3a3f4439effcf82c28e9cb9bce227c1d1fe
HeftyB <Ashields0923@gmail.com>
2021-10-21 12:55:22 -0400

    Initial Commit

---
+++ Super Supper/Super_SupperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_SupperApp.swift
+//  Super Supper
+//
+//  Created by Andrew Shields on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Super_SupperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
